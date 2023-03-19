4c13e9cb9208d174fe7f67256714d393aca964ce
Koushik1031 <94104591+Koushik1031@users.noreply.github.com>
2023-03-19 14:13:07 +0530

    Create Code9.go

---
+++ Code9.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 1
+	j := 2
+	k := 3
+
+	var c bool = true
+	var python bool = false
+	java := "no!"
+
+	fmt.Println(i, j, k, c, python, java)
+}
