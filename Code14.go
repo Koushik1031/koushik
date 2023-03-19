7ffe0bcaf8dcbeea101e75aac1cea2dd6b04f4f5
Koushik1031 <94104591+Koushik1031@users.noreply.github.com>
2023-03-19 14:20:07 +0530

    Create Code14.go

---
+++ Code14.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    var Pi float64 = 3.14
+    var World string = "世界"
+    var Truth bool = true
+    
+    fmt.Println("Hello", World)
+    fmt.Println("Happy", Pi, "Day")
+    fmt.Println("Go rules?", Truth)
+}
