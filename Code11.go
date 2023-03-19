bdb52583d7e052a86c965409d7f89ea25c4946fb
Koushik1031 <94104591+Koushik1031@users.noreply.github.com>
2023-03-19 14:16:35 +0530

    Create Code11.go

---
+++ Code11.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int // zero value is 0
+	var f float64 // zero value is 0.0
+	var b bool // zero value is false
+	var s string // zero value is empty string ""
+	fmt.Println(i)
+	fmt.Println(f)
+	fmt.Println(b)
+	fmt.Println(s)
+}
